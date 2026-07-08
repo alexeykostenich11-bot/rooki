b4ff5e317d82673ea682b5bee85aaef259dc4acb
Alexey <alexeykostenich11@gmail.com>
2026-07-08 13:43:18 +0300

    Remove unused Eve chat template code, keep only rooki site

--- agent/channels/eve.ts
+++
@@ -1,8 +0,0 @@
-import { eveChannel } from "eve/channels/eve";
-import { localDev, vercelOidc } from "eve/channels/auth";
-import { betterAuthEveAuth } from "@/lib/eve-auth";
-
-export default eveChannel({
-  auth: [betterAuthEveAuth, localDev(), vercelOidc()],
-  uploadPolicy: "disabled",
-});
